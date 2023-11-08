72f6c3ebe8db89f8eca0123bcde71cc33789094e
Gino Latorilla <gino.latorilla@ballys.com>
2023-11-08 11:08:32 +0800

    Use gin as HTTP web framework

---
+++ server/server.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type server struct {
+	version       string
+	router        http.Handler
+	listenAddress string
+}
+
+func NewServer(listenAddr string) *server {
+	server := new(server)
+	server.version = version
+	server.listenAddress = listenAddr
+
+	engine := gin.Default()
+	server.router = engine
+
+	engine.GET("/", server.handleRoot)
+
+	return server
+}
+
+func (s *server) Serve() {
+	http.ListenAndServe(s.listenAddress, s.router)
+}
+
+func (s *server) handleRoot(c *gin.Context) {
+	c.Writer.WriteString(fmt.Sprintf("Hello, World! App version: %s", s.version))
+}
