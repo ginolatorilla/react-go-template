f79b76aa59686b1a6aef5dbd814dd24b40e001ff
Gino Latorilla <pblatorilla+github@gmail.com>
2024-01-14 17:44:27 +0800

    Load .envs for the client

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,6 +5,7 @@ import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  envPrefix: "REACT_GO_TEMPLATE_",
   resolve: {
     alias: [
       { find: '@assets', replacement: path.resolve(__dirname, "./src/assets") },
