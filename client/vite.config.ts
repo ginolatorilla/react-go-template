a89b27c2209cad2621821807b32f931607574701
Gino Latorilla <pblatorilla+github@gmail.com>
2024-11-14 23:44:57 +0800

    Invoke TailwindCSS from Vite

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from 'tailwindcss'
 import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()]
+    }
+  },
   envPrefix: "REACT_GO_TEMPLATE_",
   resolve: {
     alias: [
