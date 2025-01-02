babf5778684a32779ba4d20e9bfbbc498deb0913
Gino Latorilla <pblatorilla+github@gmail.com>
2025-01-02 13:46:21 +0800

    Split middleware from server.go

--- server/server.go
+++ server/server.go
@@ -42,22 +42,6 @@ func NewServer(c Options) *server {
 	return server
 }
 
-func CORSMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.Header("Access-Control-Allow-Origin", "*")
-		c.Header("Access-Control-Allow-Credentials", "true")
-		c.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
-		c.Header("Access-Control-Allow-Methods", "OPTIONS, HEAD, POST, GET, PUT, DELETE")
-
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(204)
-			return
-		}
-
-		c.Next()
-	}
-}
-
 func (s *server) Serve() error {
 	defer zap.S().Sync()
 	zap.S().Infof("%s version: %s", s.name, s.version)
